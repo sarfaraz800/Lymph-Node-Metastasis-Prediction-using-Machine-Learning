c4947d4cc9bbb691757d822529cd831f2bd842d4
Sarfaraz Ahmad <111069645+sarfaraz800@users.noreply.github.com>
2024-10-02 23:55:59 +0530

    model.py

--- model.py
+++ model.py
@@ -1,21 +1,16 @@
-from PIL import Image
-import tensorflow as tf
-import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
+import matplotlib.pyplot as plt
 
-from tensorflow import keras
+import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+from tensorflow.data import AUTOTUNE
 
-
-
-import matplotlib.pyplot as plt
-
-data_dir= 'path'
+data_dir= r'C:\Users\HP\Desktop\Project\breast cancer flask\breast cancer flask\DataSet'
 batch_size = 32
 img_height = 180
 img_width = 180
+
 train_ds = tf.keras.utils.image_dataset_from_directory(
   data_dir,
   validation_split=0.2,
@@ -49,22 +44,22 @@ for image_batch, labels_batch in train_ds:
   print(labels_batch.shape)
   break
 
-AUTOTUNE = tf.data.AUTOTUNE
-
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-normalization_layer = layers.Rescaling(1./255)
+normalization_layer = layers.Rescaling(1./255) #Normalizes pixel values from [0, 255] to [0, 1].
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+val_ds = val_ds.map(lambda x, y: (normalization_layer(x), y))
+
+
 image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
-
-# Notice the pixel values are now in `[0,1]`.
+# Pixel values are now in [0,1].
 print(np.min(first_image), np.max(first_image))
 num_classes = len(class_names)
 
+
 model = Sequential([
-  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
@@ -81,12 +76,23 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 model.summary()
-epochs=50
+epochs=30
 history = model.fit(
-  train_ds,
+  normalized_ds,
   validation_data=val_ds,
   epochs=epochs
 )
+
+# Plot training & validation accuracy values
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+#plt.savefig('static/model_accuracy.png')  # Save the graph as an image
+plt.close()
 
 # Save the fine-tuned model
 model.save('fine_tuned_CNN.h5')
