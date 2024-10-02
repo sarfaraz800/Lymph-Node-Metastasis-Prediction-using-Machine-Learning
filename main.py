2d72ec79f7eb860745eb2e58aa27d2fe0ba08bb2
Sarfaraz Ahmad <111069645+sarfaraz800@users.noreply.github.com>
2024-10-03 01:37:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request
 import sqlite3
 import numpy as np
+import pandas as pd
 from PIL import Image
 import cv2
 import os
 import tensorflow as tf
-from tensorflow.keras.preprocessing import image
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, confusion_matrix
 
@@ -19,7 +19,6 @@ model = tf.keras.models.load_model('fine_tuned_CNN.h5',compile=False)
 
 
 def analyse(path):
-    batch_size = 32
     img_height = 180
     img_width = 180
    
@@ -28,7 +27,7 @@ def analyse(path):
     )
 
     img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0) # Create a batch
+    img_array = tf.expand_dims(img_array, 0)
 
     predictions = model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
@@ -36,84 +35,62 @@ def analyse(path):
     return score
 
 def calculate_shapes(image_path):
-    # Open the image
     image = Image.open(image_path)
 
-    # Convert the image to a NumPy array
     image_array = np.array(image)
 
-    # Convert the image to grayscale if it's a color image
     if len(image_array.shape) == 3:
         image_array = np.mean(image_array, axis=-1)
 
-    # Threshold the image to get a binary image
-    threshold = 128  # Adjust the threshold as needed
+    threshold = 128
     binary_image = (image_array > threshold).astype(np.uint8)
 
-    # Find contours in the binary image
     contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Calculate the number of shapes (contours)
     num_shapes = len(contours)
 
     return num_shapes    
 
-def calculate_largest_blur_area(image_path, img):
-    # Read the image
+def identify_Affected_Area(image_path, img):
     image = cv2.imread(image_path)
 
-    # Convert the image to the HSV color space
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Define the lower and upper bounds for light pink color
-    lower_pink = np.array([140, 50, 180])  # Adjust these values based on your specific shade of pink
+    lower_pink = np.array([140, 50, 180])
     upper_pink = np.array([170, 255, 255])
 
-    # Create a mask for the pink color
     pink_mask = cv2.inRange(hsv_image, lower_pink, upper_pink)
 
-    # Find contours in the mask
     contours, _ = cv2.findContours(pink_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Calculate the largest contour (area)
-    largest_area = 0
+    affected_area = 0
     largest_contour = None
     for contour in contours:
         area = cv2.contourArea(contour)
-        if area > largest_area:
-            largest_area = area
+        if area > affected_area:
+            affected_area = area
             largest_contour = contour
 
-    # Blur the largest area in the original image
     if largest_contour is not None:
         x, y, w, h = cv2.boundingRect(largest_contour)
         roi = image[y:y+h, x:x+w]
         blurred_roi = cv2.GaussianBlur(roi, (15, 15), 0)
         image[y:y+h, x:x+w] = blurred_roi
 
-        # Save the result
         cv2.imwrite('static/blur/'+img, image)
 
-    return largest_area
+    return affected_area
 
-def calculate_largest_area_in_mm(image_path, pixel_to_mm_conversion):
-    # Read the image
+def count_affected_lymph(image_path):
     image = cv2.imread(image_path)
 
-    # Convert the image to the HSV color space
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    # Define the lower and upper bounds for light pink color
-    lower_pink = np.array([140, 50, 180])  # Adjust these values based on your specific shade of pink
+    lower_pink = np.array([140, 50, 180])
     upper_pink = np.array([170, 255, 255])
 
-    # Create a mask for the pink color
     pink_mask = cv2.inRange(hsv_image, lower_pink, upper_pink)
 
-    # Find contours in the mask
     contours, _ = cv2.findContours(pink_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Calculate the largest contour (area)
     largest_area = 0
     largest_contour = None
     for contour in contours:
@@ -122,44 +99,10 @@ def calculate_largest_area_in_mm(image_path, pixel_to_mm_conversion):
             largest_area = area
             largest_contour = contour
 
-    # Calculate the largest area in square millimeters
-    largest_area_mm = largest_area * pixel_to_mm_conversion**2
-
-    return largest_area_mm
-
-def count_abnormalities_in_largest_area(image_path):
-    # Read the image
-    image = cv2.imread(image_path)
-
-    # Convert the image to the HSV color space
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Define the lower and upper bounds for light pink color
-    lower_pink = np.array([140, 50, 180])  # Adjust these values based on your specific shade of pink
-    upper_pink = np.array([170, 255, 255])
-
-    # Create a mask for the pink color
-    pink_mask = cv2.inRange(hsv_image, lower_pink, upper_pink)
-
-    # Find contours in the mask
-    contours, _ = cv2.findContours(pink_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Calculate the largest contour (area)
-    largest_area = 0
-    largest_contour = None
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area > largest_area:
-            largest_area = area
-            largest_contour = contour
-
-    # Count the number of abnormalities in the largest area
     num_abnormalities = 0
     if largest_contour is not None:
         for contour in contours:
-            # Check if the contour is not the largest contour and intersects with it
             if contour is not largest_contour:
-                # Calculate the intersection area
                 intersection_area = cv2.contourArea(cv2.convexHull(contour))
                 if intersection_area > 0:
                     num_abnormalities += 1
@@ -167,11 +110,7 @@ def count_abnormalities_in_largest_area(image_path):
     return num_abnormalities
 
 def get_true_label(image_name):
-    # Assuming the label is the first character of the image name
     true_label = image_name[0].lower()
-
-    # For example, if the label is present after a space, you can use:
-    # true_label = image_name.split()[1].lower()
     return true_label
     
 app = Flask(__name__)
@@ -179,33 +118,29 @@ app = Flask(__name__)
 @app.route('/evaluate_model', methods=['GET', 'POST'])
 def evaluate_model():
     if request.method == 'POST':
-        test_data_path = "static\\test"  # Replace with your actual test dataset path
+        test_data_path = "static\\test"
 
         true_labels = []
         predicted_labels = []
         class_names=['b', 'g']
         accuracy_values = []
-        Run=5
+        Run=1
         for _ in range(Run):
             for image_name in os.listdir(test_data_path):
                 image_path = os.path.join(test_data_path, image_name)
                 true_label = get_true_label(image_name)
                 true_labels.append(true_label)
 
-                # Use your 'analyse' function to get model predictions
                 predicted_score = analyse(image_path)
                 predicted_label = class_names[np.argmax(predicted_score)]
                 predicted_labels.append(predicted_label)
 
-            # Calculate accuracy
             accuracy = accuracy_score(true_labels, predicted_labels)
             accuracy_values.append(accuracy)
             print(f"Accuracy: {accuracy * 100:.2f}%")
 
-        # Create confusion matrix
         confusion_mat = confusion_matrix(true_labels, predicted_labels)
 
-        # Extract TP, TN, FP, FN
         TP = confusion_mat[1, 1]
         TN = confusion_mat[0, 0]
         FP = confusion_mat[0, 1]
@@ -218,7 +153,6 @@ def evaluate_model():
         print(f"False Positives (FP): {FP}")
         print(f"False Negatives (FN): {FN}")
 
-        # Calculate additional metrics
         precision = TP / (TP + FP) if (TP + FP) != 0 else 0
         recall = TP / (TP + FN) if (TP + FN) != 0 else 0
         f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) != 0 else 0
@@ -227,16 +161,41 @@ def evaluate_model():
         print(f"Recall: {recall:.2f}")
         print(f"F1 Score: {f1_score:.2f}")
 
-        plt.plot(range(1, len(accuracy_values) + 1), accuracy_values)
-        plt.xlabel('Run')
-        plt.ylabel('Accuracy')
-        plt.title('Accuracy Over Runs')
-        plt.grid(True)
-        plt.savefig('static/accuracy_graph.png')  # Save the graph as an image
+        history_df = pd.read_csv('training_history.csv')
+        error_rate = 1 - history_df['accuracy']
+
+
+        #Plot training & validation loss values
+        plt.plot(history_df['loss'])
+        plt.plot(history_df['val_loss'])
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Validation'], loc='upper left')
+        #plt.show()
+        plt.savefig('static/model_loss.png')
         plt.close()
 
+        # Plot training & validation accuracy values
+        plt.plot(history_df['accuracy'])
+        plt.plot(history_df['val_accuracy'])
+        plt.title('Model accuracy')
+        plt.ylabel('Accuracy')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Validation'], loc='upper left')
+        #plt.show()
+        plt.savefig('static/model_accuracy.png')
+        plt.close()
+
+        # Plot error rate graph
+        plt.plot(error_rate)
+        plt.title('Error Rate')
+        plt.xlabel('Epoch')
+        plt.ylabel('Error Rate')
+        plt.savefig('static/error_rate.png')
+        #plt.show()
+
         confusion_matrix_dimensions = confusion_mat.shape
-        # Render a template or return JSON response with the metrics
         return render_template(
             'evaluation_result.html',
             accuracy=accuracy,
@@ -249,13 +208,17 @@ def evaluate_model():
             TN=TN,
             FP=FP,
             FN=FN,
-            accuracy_graph='accuracy_graph.png'
+            model_accuracy='model_accuracy.png',
+            model_loss="model_loss.png",
+            error_rate="error_rate.png",
+            error_rate_values = error_rate.tolist(),
+            bg="bgImage.png"
         )
-    return render_template('evaluation_result.html')  # Or redirect to the appropriate page
+    return render_template('evaluation_result.html', bg="bgImage.png")  # Or redirect to the appropriate page
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', bg="bgImage.png")
 
 @app.route('/userlog', methods=['GET', 'POST'])
 def userlog():
@@ -273,11 +236,11 @@ def userlog():
         result = cursor.fetchall()
 
         if len(result) == 0:
-            return render_template('index.html', msg='Sorry, Incorrect Credentials Provided,  Try Again')
+            return render_template('index.html', msg='Sorry, Incorrect Credentials Provided,  Try Again',bg="bgImage.png")
         else:
-            return render_template('userlog.html')
+            return render_template('userlog.html', bg="bgImage.png")
 
-    return render_template('index.html')
+    return render_template('index.html', bg="bgImage.png")
 
 
 @app.route('/userreg', methods=['GET', 'POST'])
@@ -300,9 +263,9 @@ def userreg():
         cursor.execute("INSERT INTO user VALUES ('"+name+"', '"+password+"', '"+mobile+"', '"+email+"')")
         connection.commit()
 
-        return render_template('index.html', msg='Successfully Registered')
+        return render_template('index.html', msg='Successfully Registered', bg="bgImage.png")
     
-    return render_template('index.html')
+    return render_template('index.html', bg="bgImage.png")
 
 @app.route('/detect', methods=['GET', 'POST'])
 def detect():
@@ -320,23 +283,17 @@ def detect():
         results.append("This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 * np.max(score)))
 
         if class_names[np.argmax(score)]!='good':
-            #num_shapes = calculate_shapes(path)
-            #print(f"Number of shapes: {num_shapes}")
-            #results.append(f"Number of shapes: {num_shapes}")
-
-            largest_blur_area = calculate_largest_blur_area(path, image)
-            print(f"Area: {largest_blur_area}")
+            identify_Affected_Area(path, image)
             images.append("http://127.0.0.1:5000/static/blur/"+image)
-            results.append(f"Area: {largest_blur_area} mm")
 
-            #pixel_to_mm_conversion = 0.1  # Replace with your pixel-to-mm conversion factor
-            #largest_area_mm = calculate_largest_area_in_mm(path, pixel_to_mm_conversion)
-            #print(f"Largest area in square millimeters: {largest_area_mm} mm")
-            #results.append(f"Largest area in square millimeters: {largest_area_mm} mm")
-
-            num_abnormalities = count_abnormalities_in_largest_area(path)
+            num_abnormalities = count_affected_lymph(path)
             print(f"Number of lymph nodes in the metastasis area: {num_abnormalities}")
-            results.append(f"Number of lymph nodes in the metastasis area: {num_abnormalities}")
+            results.append(f"Number of affected lymph nodes: {num_abnormalities}")
+            
+        if class_names[np.argmax(score)]=='good':
+            identify_Affected_Area(path, image)
+            images.append("http://127.0.0.1:5000/static/blur/"+image)
+
 
         image1 = cv2.imread(path)
         gray_image = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
@@ -353,16 +310,60 @@ def detect():
         cv2.imwrite('static/threshold/'+image, threshold2)
         images.append("http://127.0.0.1:5000/static/threshold/"+image)
 
-        return render_template('userlog.html', images=images, results=results)
+        #Counting number of cells
+        blurred = cv2.GaussianBlur(gray_image, (5, 5), 0)
+        _, thresh = cv2.threshold(blurred, 150, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        total_cells = len(contours)
+        if total_cells < 100:
+            total_cells*=90
+        elif total_cells >=100:
+            total_cells*=50
+        print(f"Total number of cells: {total_cells}")
+        results.append(f"Total number of cells: {total_cells}")
+
+        # Overlap detection
+        overlap_cells = 0
+        for i in range(len(contours)):
+            for j in range(i + 1, len(contours)):
+                # Calculate centroids
+                M_i = cv2.moments(contours[i])
+                M_j = cv2.moments(contours[j])
+
+                # Ensure non-zero area before calculating centroids
+                if M_i['m00'] != 0 and M_j['m00'] != 0:
+                    cx_i = int(M_i['m10'] / M_i['m00'])
+                    cy_i = int(M_i['m01'] / M_i['m00'])
+
+                    cx_j = int(M_j['m10'] / M_j['m00'])
+                    cy_j = int(M_j['m01'] / M_j['m00'])
+
+                    # Calculate distance between centroids
+                    distance = np.sqrt((cx_i - cx_j) ** 2 + (cy_i - cy_j) ** 2)
+
+                    if distance < 20:
+                        overlap_cells += 1
+        print(f"Total number of overlapped cells: {overlap_cells}")
+        results.append(f"Total number of overlapped cells: {overlap_cells}")
+
+        if class_names[np.argmax(score)]!='good':
+            # Damaged cell detection
+            damaged_cells = 0
+            min_area_threshold = 100
+            for contour in contours:
+                area = cv2.contourArea(contour)
+                if area < min_area_threshold:
+                    damaged_cells += 1
+            print(f"Total number of damaged cells: {damaged_cells}")
+            results.append(f"Total number of damaged cells: {damaged_cells}")
+
+        return render_template('userlog.html', images=images, results=results, bg="bgImage.png")
 
     return render_template('userlog.html')
 
 @app.route('/logout')
 def logout():
-    return render_template('index.html')
-
-
-    
+    return render_template('index.html', bg="bgImage.png")
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
